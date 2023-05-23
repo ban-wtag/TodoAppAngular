771787d8d4a984e9c7f292effa977fcb2a8fd1af
ban-wtag <129286746+ban-wtag@users.noreply.github.com>
2023-05-23 19:55:41 +0600

    style input box and input buttons

--- src/app/components/card-item/card-item-routing.module.ts
+++ src/app/components/card-item/card-item-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskButtonModule } from 'src/app/components/task-button/task-button.module';
 
 const routes: Routes = [
     { path: 'taskButton', loadChildren: () => import('src/app/components/task-button/task-button.module').then(m => m.TaskButtonModule) }
-    // {path: 'taskButton', loadChildren: 'src/app/components/task-button/task-button.module#TaskButtonModule' }
 ];
 
 @NgModule({
